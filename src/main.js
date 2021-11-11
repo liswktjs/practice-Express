d777ccc485532d23c75f6fb446a4176589101126
liswktjs <60773373+liswktjs@users.noreply.github.com>
2021-11-11 11:57:24 +0900

    미들웨어

--- src/main.js
+++ src/main.js
@@ -1,14 +1,32 @@
 //@ts-check
 
 const express = require('express')
+const fs = require('fs')
 
 const app = express()
 
 const PORT = 5000
 
-app.use('/', (req, res) => {
-  res.send('Hello express')
+app.use('/', async (req, res, next) => {
+  console.log('middleware 1')
+  const fileContent = await fs.promises.readFile('.gitignore')
+  const requestedAt = new Date()
+  //@ts-ignore
+  req.requestedAt = requestedAt
+  //@ts-ignore
+  req.fileContent = fileContent
+  next()
 })
+
+app.use((req, res) => {
+  console.log('middleware 2')
+
+  res.send(
+    //@ts-ignore
+    `Hello express! : requested at ${req.requestedAt}, ${req.fileContent}`
+  )
+})
+
 app.listen(PORT, () => {
   console.log('The express server is working')
 })
